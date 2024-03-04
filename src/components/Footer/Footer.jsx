4dce094325172f4556b6e9af384c52132efe25be
kaiovinic <kaiovini19@hotmail.com>
2024-03-04 00:33:54 -0300

    Ajeitando footer responsivp md

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
     <footer id="contato" className="w-full h-screen bg-white">
       <section
         className={`${styles.paralax01} w-full bg-slate-900 text-black p-4 flex flex-row justify-between overflow-hidden bg-no-repeat
-        bg-fixed shadow-xl max-md:flex-col max-sm:justify-start`}
+        bg-fixed shadow-xl max-md:flex-col max-sm:justify-start `}
         id="contato"
       >
         <nav>
@@ -35,7 +35,7 @@ const Footer = () => {
             <img
               src={logo}
               alt="logo"
-              className="w-80 max-md:w-60 max-md:mb-10"
+              className="w-80 max-md:w-60 max-md:mb-4 max-lg:w-40"
             />
           </Link>
         </nav>
@@ -44,7 +44,7 @@ const Footer = () => {
           <p className="text-2xl font-bold mr-10 mb-1 max-md:text-xl">
             Me acompanhe em minhas redes sociais:
           </p>
-          <ul className="flex gap-x-14 mt-10 text-4xl ml-20 max-md:text-2xl max-md:ml-0">
+          <ul className="flex gap-x-14 mt-10 text-4xl ml-20 max-md:text-2xl max-md:ml-0 max-md:mt-4">
             <li className="hover:text-[#816e26]">
               <a
                 href="https://www.tiktok.com/@karenvanesca"
@@ -82,7 +82,7 @@ const Footer = () => {
               </a>
             </li>
           </ul>
-          <div className="flex flex-col mt-10">
+          <div className="flex flex-col mt-10 max-md:mt-0">
             <a
               href="mailto:karen.vanesca@hotmail.com"
               className="text-3xl font-bold mt-10 flex gap-2 hover:text-[#816e26] max-md:text-2xl"
@@ -97,7 +97,7 @@ const Footer = () => {
               https://api.whatsapp.com/send?phone=5521983932168&text=Oi%20Karen,%20estou%20passando%20por%20um%20grande%20desafio%20profissional%20e%20preciso%20da%20sua%20ajuda!%20"
               target="_blank"
               rel="noreferrer"
-              className="text-3xl font-bold mt-10 flex gap-2 hover:text-[#816e26] max-md:text-2xl"
+              className="text-3xl font-bold mt-10 flex gap-2 hover:text-[#816e26] max-md:text-2xl max-md:mt-4"
             >
               <FaWhatsapp className="text-green-500" />
               +55 (21)98393-2168
